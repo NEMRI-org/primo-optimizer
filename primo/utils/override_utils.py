717233878e7893f25042104eb1d27af64a36dcf8
Yash Puranik <yash.puranik@aimpointdigital.com>
2024-09-02 22:33:52 -0400

    WIP: Recalculate feature

--- primo/utils/override_utils.py
+++ primo/utils/override_utils.py
@@ -13,7 +13,7 @@
 
 # Standard libs
 import logging
-from typing import Dict, Union
+from typing import Dict, List, Optional
 
 # Installed libs
 import pandas as pd
@@ -21,22 +21,9 @@ from haversine import Unit, haversine_vector
 
 # User-defined libs
 from primo.data_parser.data_model import OptInputs
-from primo.utils.config_utils import OverrideWidget
+from primo.utils.raise_exception import raise_exception
 
 LOGGER = logging.getLogger(__name__)
-
-
-def user_input(well_df_add: pd.DataFrame, well_df_remove: pd.DataFrame):
-    """
-    A wrapper for generating a widget that facilitates the addition and removal of wells
-
-    """
-    widget_add = OverrideWidget(well_df_add, "Add the well to the suggested projects")
-    widget_remove = OverrideWidget(
-        well_df_remove, "Remove the well from the suggested projects"
-    )
-
-    return widget_add, widget_remove
 
 
 class Recalculate:
@@ -47,170 +34,182 @@ class Recalculate:
     Parameters
     ----------
 
-    original_well_list : pd.DataFrame
+    selected_wells : pd.DataFrame
         A DataFrame containing wells selected based on solving the optimization problem
-        without considering the override wells.
+        and/or by manual selections and overrides
 
-    well_add_list : list
+    wells_added : list
         A list of wells that the user wishes to add to the P&A projects
 
-    well_remove_list : list
+    wells_removed : list
         A list of wells that the user wishes to remove from the P&A projects
 
     well_df : pd.DataFrame
         A DataFrame that includes all candidate wells
 
-    mobilization_costs : Dict[int, float]
-        A dictionary mapping well IDs to mobilization costs. These costs should match
-        those used in the optimization problem.
-
-    budget : int
-        An integer for the available budget for plugging wells
-
-    max_wells_per_owner : int
-        An integer for the maximum number of wells an owner can have in the overall
-        P&A projects. This value should align with the one used in the optimization problem.
-
-    max_distance : int
-        An integer for the maximum allowable distance between two wells within the
-        same project. This value should match the one used in the optimization problem.
+    opt_inputs : OptInputs
+        Input object for the optimization problem
 
     dac_weight : int
         An integer for the weight assigned to the DAC priority factor.
 
-    dac_budget_fraction : int
-        An integer for the minimum percentage of wells to be plugged that should
-        be located in DAC areas. This value should align with the one used in the
-        optimization problem.
-
     Attributes
     ----------
 
-    violate_cost : Union[int, bool]
-        The actual budget if the budget constraint is violated; otherwise False
+    original_wells : pd.DataFrame
+        List of original wells selected for plugging
 
-    violate_dac : Union[int, bool]
-        The actual DAC percentage if the DAC constraint is violated otherwise False
+    wells_added : List[str]
+        List of wells to be added for plugging
 
-    violate_operator : dict
-        A dictionary providing information on owners who exceed the well count constraint.
-        Includes owner name, the number of wells belonging to the owner in the project,
-        and the specific well IDs.
+    wells_removed : List[str]
+        List of wells to be removed from plugging projects
 
-    violate_distance : dict
-        A dictionary detailing wells that breach the maximum distance constraint.
-        Includes the project, well pairs, and the actual distances between these well pairs.
+    well_df : pd.DataFrame
+        Full data associated with all wells
+
+    opt_inputs : OptInputs
+        The inputs associated with the optimization problem
+
+    dac_weight : Union[float, None]
+        An integer for the weight assigned to the DAC priority factor.
 
     """
 
+    # pylint: disable=too-many-arguments
     def __init__(
         self,
-        original_well_list: pd.DataFrame,
-        well_add_list: list,
-        well_remove_list: list,
+        original_wells: pd.DataFrame,
+        wells_added: List[str],
+        wells_removed: List[str],
         well_df: pd.DataFrame,
         opt_inputs: OptInputs,
-        dac_weight: float = None,
+        dac_weight: Optional[float] = None,
     ):
-        self.original_well_list = original_well_list
-        self.well_add_list = well_add_list
-        self.well_remove_list = well_remove_list
+        self.original_wells = original_wells
+        self.wells_added = [int(well_id) for well_id in wells_added]
+        self.wells_removed = [int(well_id) for well_id in wells_removed]
         self.well_df = well_df
-        self.mobilization_costs = opt_inputs.mobilization_cost
-        self.budget = opt_inputs.budget
-        self.dac_budget_fraction = opt_inputs.dac_budget_fraction
-        self.max_wells_per_owner = opt_inputs.max_wells_per_owner
-        self.max_distance = opt_inputs.distance_threshold
+        self.opt_inputs = opt_inputs
         self.dac_weight = dac_weight
-        self.violate_cost = 0
-        self.violate_dac = 0
 
-        for well_id in self.well_add_list:
-            well_id = int(well_id)
-            self.well_add = self.well_df[self.well_df["API Well Number"] == well_id]
-            self.original_well_list = pd.concat(
-                [self.original_well_list, self.well_add]
-            )
+        # Get list of wells to be plugged (opt results + user overrides)
+        added_wells = well_df[well_df["API Well Number"].isin(self.wells_added)]
+        self._plugged_list = pd.concat([original_wells.copy(), added_wells])
 
-        well_remove_list = [int(well_id) for well_id in self.well_remove_list]
-        self.original_well_list["drop"] = self.original_well_list.apply(
-            lambda row: 0 if row["API Well Number"] in well_remove_list else 1,
-            axis=1,
-        )
-        self.well_return_df = self.original_well_list[
-            self.original_well_list["drop"] == 1
+        # Get list of wells to be removed from plugging
+        self._plugged_list = self._plugged_list[
+            ~self._plugged_list["API Well Number"].isin(self.wells_removed)
         ]
 
-    def budget_assess(self) -> Union[int, bool]:
+    def _add_well(self, well_id: int):
         """
-        Assess whether the budget constraint is violated or not
+        Adds a well in the considerations for plugging
+        """
+        if well_id in self.wells_added:
+            LOGGER.warning(f"Well: {well_id} was already included in plugging list")
+            return
+
+        if well_id in self.wells_removed:
+            raise_exception(
+                f"Well: {well_id} was already included in removal list", ValueError
+            )
+
+        self.wells_added.append(well_id)
+        self._plugged_list = pd.concat(
+            [
+                self._plugged_list,
+                self.well_df[self.well_df["API Well Number"] == well_id],
+            ]
+        )
+        return
+
+    def _remove_well(self, well_id: int):
+        """
+        Removes a well from the considerations for plugging
+        """
+        if well_id in self.wells_removed:
+            raise_exception(
+                f"Well: {well_id} was already included in removal list", ValueError
+            )
+
+        if well_id in self.wells_added:
+            self.wells_added.remove(well_id)
+
+        self._plugged_list = self._plugged_list[
+            self._plugged_list["API Well Number"] != well_id
+        ]
+
+    # TODO: Move these assessments elsewhere as they can be reused
+    def assess_budget(self) -> float:
+        """
+        Assesses the impact on budget after user overrides and returns the
+        amount by which the budget is violated. A 0 or negative value indicates
+        that we are still under budget
         """
         total_cost = 0
-        for _, groups in self.well_return_df.groupby("Project"):
+        for _, groups in self._plugged_list.groupby("Project"):
             n_wells = len(groups)
-            campaign_cost = self.mobilization_costs[n_wells]
+            campaign_cost = self.opt_inputs.mobilization_cost[n_wells]
             total_cost += campaign_cost
-        if total_cost > self.budget:
-            self.violate_cost = total_cost
-        else:
-            self.violate_cost = False
-        return self.violate_cost
 
-    def dac_assess(self) -> Union[int, bool]:
-        """
-        Assess whether the DAC constraint is violated or not
-        """
-        for _, row in self.well_return_df.iterrows():
-            well_id = row["API Well Number"]
-            if self.dac_weight is not None:
-                threshold = self.dac_weight / 100 * self.dac_budget_fraction
-                disadvantaged_community_score = row[
-                    f"DAC Score [0-{int(self.dac_weight)}]"
-                ]
-                is_disadvantaged = float(disadvantaged_community_score > threshold)
-            else:
-                # When the user does not select DAC as a priority factor,
-                # all wells are assumed to not be located in a disadvantaged community.
-                is_disadvantaged = float(False)
-            self.well_return_df.loc[
-                self.well_return_df["API Well Number"] == well_id, ["In DAC"]
-            ] = is_disadvantaged
-        dac_percent = (
-            self.well_return_df["In DAC"].sum() / len(self.well_return_df) * 100
-        )
-        if dac_percent < self.dac_budget_fraction:
-            self.violate_dac = dac_percent
-        else:
-            self.violate_dac = False
-        return self.violate_dac
+        return self.opt_inputs.budget - total_cost
 
-    def operator_assess(self) -> Dict:
+    def assess_dac(self) -> float:
         """
-        Assess whether the owner well count constraint is violated or not
+        Assess whether the DAC constraint is violated or not and returns
+        the amount by which the DAC constraint is violated. A 0 or negative value
+        indicates that constraint is satisfied
         """
-        self.violate_operator = {}
-        for operator, groups in self.well_return_df.groupby("Operator Name"):
+        opt_inputs = self.opt_inputs
+        if self.dac_weight is None:
+            # When the user does not select DAC as a priority factor,
+            # all wells are assumed to not be located in a disadvantaged community.
+            return opt_inputs.dac_budget_fraction
+
+        disadvantaged_wells = 0
+        col_name = f"DAC Score [0-{int(self.dac_weight)}]"
+        threshold = self.dac_weight / 100 * opt_inputs.dac_budget_fraction
+
+        # count number of wells that exceed threshold based on disadvantaged
+        # community score
+        disadvantaged_wells = (self._plugged_list[col_name] > threshold).sum()
+        dac_percent = disadvantaged_wells * 100 / self._plugged_list.shape[0]
+
+        return opt_inputs.dac_budget_fraction - dac_percent
+
+    def assess_owner_well_count(self) -> Dict:
+        """
+        Assess whether the owner well count constraint is violated or not.
+        Returns list of owners and wells selected for each for whom the owner
+        well count constraint is violated
+        """
+        violated_operators = {}
+        for operator, groups in self._plugged_list.groupby("Operator Name"):
             n_wells = len(groups)
-            if n_wells > self.max_wells_per_owner:
-                self.violate_operator[operator] = [
+            if n_wells > self.opt_inputs.max_wells_per_owner:
+                violated_operators[operator] = [
                     n_wells,
                     groups["API Well Number"].to_list(),
                 ]
 
-        return self.violate_operator
+        return violated_operators
 
-    def distance_asses(self) -> Dict:
+    def assess_distances(self) -> Dict:
         """
         Assess whether the maximum distance between two wells constraint is violated or not
         """
-        self.violate_distance = {}
-        for cluster_id, groups in self.well_return_df.groupby("Project"):
+        distance_threshold = self.opt_inputs.distance_threshold
+        distance_violations = {}
+        # TODO: Consider calculating these once and for all since back filling
+        # might end up causing multiple iterations here
+        for cluster_id, groups in self._plugged_list.groupby("Project"):
             well_id = list(groups["API Well Number"])
             num_well = len(well_id)
-            groups["coor"] = list(zip(groups.Latitude, groups.Longitude))
+            groups["coords"] = list(zip(groups.Latitude, groups.Longitude))
             distance_metric_distance = haversine_vector(
-                groups["coor"].to_list(),
-                groups["coor"].to_list(),
+                groups["coords"].to_list(),
+                groups["coords"].to_list(),
                 unit=Unit.MILES,
                 comb=True,
             )
@@ -218,8 +217,8 @@ class Recalculate:
             for well_1 in range(num_well - 1):
                 for well_2 in range(well_1 + 1, num_well):
                     well_distance = distance_metric_distance[well_1][well_2]
-                    if well_distance > self.max_distance:
-                        self.violate_distance[
+                    if well_distance > distance_threshold:
+                        distance_violations[
                             (cluster_id, well_id[well_1], well_id[well_2])
                         ] = [
                             cluster_id,
@@ -228,65 +227,63 @@ class Recalculate:
                             well_distance,
                         ]
 
-        return self.violate_distance
+        return distance_violations
 
+    def assess_feasibility(self) -> bool:
+        """
+        Assesses whether current set of selections is feasible
+        """
+        if self.assess_budget() > 0:
+            return False
 
-def back_fill(
-    selected_well,
-    well_add_list_well,
-    well_remove_list_well,
-    well_gdf,
-    opt_inputs_well,
-    well,
-):
-    violation = Recalculate(
-        selected_well,
-        well_add_list_well,
-        well_remove_list_well,
-        well_gdf,
-        opt_inputs_well,
-    )
-    violate_cost = violation.budget_assess()
-    violate_operator = violation.operator_assess()
-    violate_distance = violation.distance_asses()
-    if (
-        violate_cost is False
-        and bool(violate_operator) is False
-        and bool(violate_distance) is False
-    ):
-        return [well]
+        if self.assess_dac() > 0:
+            return False
 
+        if self.assess_owner_well_count():
+            return False
 
-def well_candidates_list(
-    well_violate,
-    well_gdf,
-    selected_well,
-    well_add_list_well,
-    well_remove_list_well,
-    opt_inputs_well,
-):
-    well_candidates_dict = {}
-    for well_id in well_violate:
-        well_backfill = []
-        cluster = int(well_gdf[well_gdf["API Well Number"] == well_id]["Project"])
-        well_candidates = well_gdf[
-            (well_gdf["Project"] == str(cluster))
-            & (well_gdf["API Well Number"] != well_id)
-        ]["API Well Number"]
-        for well in well_candidates:
-            well_add_list_well_drop = [
-                int(id) for id in well_add_list_well if int(id) not in well_violate
-            ]
-            well_add_list_well_drop += [well]
-            well_candidate = back_fill(
-                selected_well,
-                well_add_list_well_drop,
-                well_remove_list_well,
-                well_gdf,
-                opt_inputs_well,
-                well,
-            )
-            if well_candidate is not None:
-                well_backfill += well_candidate
-        well_candidates_dict[well_id] = well_backfill
-    return well_candidates_dict
+        if self.assess_distances():
+            return False
+
+        return True
+
+    def backfill(self) -> List[str]:
+        """
+        If current selections (either due to manual overrides or termination of solver
+        prior to optimality)
+        leave room for budget, the method returns a list of candidates that can
+        be added to the plugging projects without violating any constraints
+
+        Parameters:
+        ----------
+        None
+
+        Returns:
+        --------
+        List of Well API numbers that can be added in the project
+        """
+        additions = []
+        if self.assess_feasibility is False:
+            # The
+            return additions
+
+        # Sort by descending order
+        candidates = self.well_df.sort_values("Priority Score [0-100]", ascending=False)
+
+        # Only include those candidates not considered for plugging
+        candidates = candidates[
+            ~candidates["API Well Number"].isin(self._plugged_list["API Well Number"])
+        ]
+
+        # Stick to those projects that already exist
+        existing_projects = set(self._plugged_list["Project"])
+        candidates = candidates[candidates["Project"].isin(existing_projects)]
+
+        for well_id in candidates["API Well Number"]:
+            self._add_well(well_id)
+            if self.assess_feasibility():
+                additions.append(well_id)
+            else:
+                self._remove_well(well_id)
+
+        return additions
